11145ba1d1b56a37509d9e1a3309373178266eee
Christin Schade <christinabbe90gmail.com>
2022-08-22 16:29:40 +0200

    feat(index.html style.css) content and first styling

---
+++ main.js
@@ -0,0 +1,16 @@
+let myNodelist = document.getElementsByTagName("li");
+for (let i = 0; i < myNodelist.length; i++) {
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+let close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    let div = this.parentElement;
+    div.style.display = "none";
+  }
+}
